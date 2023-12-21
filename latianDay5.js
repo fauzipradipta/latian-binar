0c52747059e1411b56d674298f665b0c84fd106f
fauzipradipta <dipta0988@gmail.com>
2023-12-21 21:25:56 +0700

    variable

--- latianDay5.js
+++ latianDay5.js
@@ -1,1 +1,17 @@
 console.log("BINAR");
+
+//challenge variable manipulasi string 
+//buat file namefile challenge-variable.js atau ariable.js
+//tampilkan data menggunakan
+/*
+let namaDepan = "Samsul"
+let namaBelakang = "Rijal 
+
+output = hallo nama aku Adit aku sedang belajar javascript
+*/
+
+//challenge tampilkan waktu
+
+//challenge array object
+
+//challenge function
---
+++ variable.js
@@ -0,0 +1,13 @@
+//challenge 1
+let namaDepan = "Syawadhilah";
+let namaBelakang = "Pradipta";
+let fullName = "halo nama saya "+ namaDepan + " " + namaBelakang
+
+console.log(fullName);
+
+//challenge2
+let data ="Halo nama saya Adit saya sedang belajar javascript"; 
+
+let hasil = data.replaceAll('saya', 'aku');
+
+console.log(hasil);
